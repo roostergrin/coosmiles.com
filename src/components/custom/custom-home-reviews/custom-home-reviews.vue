d9e2ce9d970aadb45bb90600cf3cd767b5fcd8be
mattlaguardia <matthewlaguardia@gmail.com>
2018-04-10 14:28:46 -0700

    feat(home): adds basic functionality to homepage

--- src/components/custom/custom-home-reviews/custom-home-reviews.vue
+++ src/components/custom/custom-home-reviews/custom-home-reviews.vue
@@ -1,7 +1,11 @@
-<template lang="pug" src="./custom-home-reviews.pug"></template>
+s<template lang="pug" src="./custom-home-reviews.pug"></template>
 
 <script>
+import SliderHomeTestimonials from 'components/slider/slider-home-testimonials/slider-home-testimonials'
 export default {
-  props: ['props']
+  props: ['props'],
+  components: {
+    SliderHomeTestimonials
+  }
 }
 </script>
---
+++ src/components/slider/slider-home-testimonials/slider-home-testimonials.vue
@@ -0,0 +1,7 @@
+<template lang="pug" src="./slider-home-testimonials.pug"></template>
+
+<script>
+export default {
+  props: ['props']
+}
+</script>
