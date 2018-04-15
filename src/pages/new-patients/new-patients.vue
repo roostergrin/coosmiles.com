a74d630925172529772386a71c3471402daae2a0
Mike <mcheng.oc@gmail.com>
2018-04-15 16:41:09 -0700

    feat/page-new-patients-and-about: Style pages

--- src/pages/new-patients/new-patients.vue
+++ src/pages/new-patients/new-patients.vue
@@ -1,18 +1,28 @@
 <template lang="pug" src="./new-patients.pug"></template>
 
 <script>
+import Loader from 'components/loader/loader'
+import HeroPages from 'components/hero/hero-pages/hero-pages'
 import CustomPatientsSteps from 'components/custom/custom-patients-steps/custom-patients-steps'
 import CustomBeforeAfter from 'components/custom/custom-patients-before-after/custom-patients-before-after'
 
 export default {
   computed: {
     props () {
-      return this.$state.store.pages['new-patients']
+      return this.$store.state.pages['new-patients']
+    },
+    loading () {
+      return this.$store.state.loading
     }
   },
   components: {
+    Loader,
+    HeroPages,
     CustomPatientsSteps,
     CustomBeforeAfter
+  },
+  created () {
+    console.log(this.props)
   }
 }
 </script>
