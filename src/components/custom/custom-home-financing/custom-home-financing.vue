640834dd438e97ac5906e2b58de959c62eda40c9
Mike <mcheng.oc@gmail.com>
2018-04-17 09:51:48 -0700

    feat/home-form: Create form for homepage

--- src/components/custom/custom-home-financing/custom-home-financing.vue
+++ src/components/custom/custom-home-financing/custom-home-financing.vue
@@ -1,7 +1,12 @@
 <template lang="pug" src="./custom-home-financing.pug"></template>
 
 <script>
+import HomeForm from 'components/form/form-home/form-home'
+
 export default {
-  props: ['props']
+  props: ['props'],
+  components: {
+    HomeForm
+  }
 }
 </script>
---
+++ src/components/form/form-home/form-home.vue
@@ -0,0 +1,30 @@
+<template lang='pug' src='./form-home.pug'></template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      message: '',
+      fullname: '',
+      email: '',
+      phone: '',
+      formSubmitted: false
+    }
+  },
+  methods: {
+    validateBeforeSubmit () {
+      this.$validator.validateAll()
+      .then(result => {
+        if (result) {
+          this.formSubmitted = true
+        } else {
+          alert('Correct them errors!')
+        }
+      })
+      .catch(() => {
+        alert('Correct them errors!')
+      })
+    }
+  }
+}
+</script>
