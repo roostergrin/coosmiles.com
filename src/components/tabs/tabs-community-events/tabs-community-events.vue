4405a929b6184c7434121a392effabb556ca80b7
mattlaguardia <matthewlaguardia@gmail.com>
2018-04-17 16:27:08 -0700

    feat(community): creates events and community section

--- src/components/tabs/tabs-community-events/tabs-community-events.vue
+++ src/components/tabs/tabs-community-events/tabs-community-events.vue
@@ -1,7 +1,12 @@
 <template lang="pug" src="./tabs-community-events.pug"></template>
 
 <script>
+import BlockCircles from 'components/block/block-circles/block-circles'
+
 export default {
-  props: ['props']
+  props: ['props'],
+  components: {
+    BlockCircles
+  }
 }
 </script>
--- src/shared/store/actions.js
+++ src/shared/store/actions.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import api from 'api'
 import {
   GET_PAGES,
-  // GET_BLOG,
+  GET_BLOG,
   GET_APP,
   VIEW_NAV,
   VIEW_BODY,
@@ -38,15 +38,23 @@ const actions = {
       } catch (e) { console.log('APP API: ' + e) }
     })()
   },
-  // GET_BLOG ({ commit }) {
-  //   (async () => {
-  //     try {
-  //       const response = await axios.get(`${api}/wp/v2/posts?per_page=10&_embed`)
-  //       commit(GET_BLOG, response)
-  //     } catch (e) {
-  //       console.log(e)
-  //     }
-  //   })()
+  GET_BLOG ({ commit }) {
+    (async () => {
+      try {
+        const response = await axios.get(`${api}/wp/v2/posts?per_page=100&_embed`)
+        const data = response.data.reduce(
+          (allData, data) => ({ ...allData, [data.slug]: data }),
+          {}
+        )
+        // console.log(response.data)
+        commit(GET_BLOG, data)
+      } catch (e) {
+        console.log(e)
+      }
+    })()
+  },
+  // GET_MORE ({ commit }) {
+  //   const response = axios.get(`${api}/wp/v2/posts?per_page=4&offset=4`)
   // },
   VIEW_NAV ({ commit }, data) {
     commit(VIEW_NAV, data)
