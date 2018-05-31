7d04c3592856a7d2f530efa68cf68fcaf128704a
Mike <mcheng.oc@gmail.com>
2018-05-30 22:24:41 -0700

    style/page-contact: Style contact page with new design

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -1,4 +1,4 @@
-const route = '//coosmiles.roostertest3.com/wp-json'
+const route = 'https://coosmiles.com/wp-json'
 
 const api = () => {
   return document.location.hostname === 'localhost' || document.location.href.indexOf('roostertest3') > -1 ? route : document.location.protocol + '/wp-json'
