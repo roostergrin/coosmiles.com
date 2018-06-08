46570644334c9d1e483e45ba49ec35ae3f2e762f
conmart <connor.a.martinelli@gmail.com>
2018-06-08 10:33:33 -0700

    added orthoChats code to index.php

--- dist/index.php
+++ dist/index.php
@@ -22,5 +22,23 @@
       s.parentNode.insertBefore(wf, s);
     })();
   </script>
+  <!-- BoldChat Visitor Monitor HTML v5.00 (Website=1488 - Central Ohio Orthodontics,ChatButton=1488 - Central Ohio Orthodontics,ChatInvitation=1488 - Central Ohio Orthodontics) -->
+  <script type="text/javascript">
+  window._bcvma = window._bcvma || [];
+  _bcvma.push(["setAccountID", "438176494458224912"]); _bcvma.push(["setParameter", "WebsiteID", "435751087274695686"]); _bcvma.push(["setParameter", "InvitationID", "435751087229317678"]); _bcvma.push(["setParameter", "VisitName", ""]); _bcvma.push(["setParameter", "VisitPhone", ""]); _bcvma.push(["setParameter", "VisitEmail", ""]); _bcvma.push(["setParameter", "VisitRef", ""]); _bcvma.push(["setParameter", "VisitInfo", ""]); _bcvma.push(["setParameter", "CustomUrl", ""]); _bcvma.push(["setParameter", "WindowParameters", ""]); _bcvma.push(["addFloat", {type: "chat", id: "435751090068266797"}]); _bcvma.push(["pageViewed"]);
+  var bcLoad = function(){
+  if(window.bcLoaded) return; window.bcLoaded = true;
+  var vms = document.createElement("script"); vms.type = "text/javascript"; vms.async = true; vms.src = ('https:'==document.location.protocol?'https://':'http://') +
+  "vmss.boldchat.com/aid/438176494458224912/bc.vms4/vms.js";
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(vms, s);
+  };
+  if(window.pageViewer && pageViewer.load) pageViewer.load();
+  else if(document.readyState=="complete") bcLoad();
+  else if(window.addEventListener) window.addEventListener('load', bcLoad, false); else window.attachEvent('onload', bcLoad);
+  </script>
+  <noscript>
+  <a href="http://www.boldchat.com" title="Live Chat Software" target="_blank"><img alt="Live Chat Software" src="https://vms.boldchat.com/aid/438176494458224912/bc.vmi?wdid=435751087274695686&amp;vr=&amp;vn=&amp;vi=& amp;ve=&amp;vp=&amp;curl=" border="0" width="1" height="1" /></a>
+  </noscript>
+  <!-- /BoldChat Visitor Monitor HTML v5.00 -->
 </body>
 </html>
